afaad5da196279798aedff322d257379de8882d4
Sun Tan <sutan@redhat.com>
2018-08-28 15:41:02 +0000

    Updating license to EPL-2.0
    Signed-off-by: Sun Tan <sutan@redhat.com>

--- src/node/che-workspace-hosted-plugin-uri-post-processor.ts
+++ src/node/che-workspace-hosted-plugin-uri-post-processor.ts
@@ -1,9 +1,12 @@
-/*
- * Copyright (C) 2018 Red Hat, Inc. and others.
+/*********************************************************************
+ * Copyright (c) 2018 Red Hat, Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- */
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ **********************************************************************/
 
 import { injectable } from "inversify";
 import URI from "@theia/core/lib/common/uri";
